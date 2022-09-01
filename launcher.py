519a10a4bc99dae5308ad08c0bb9e5c7514da71d
akira <62818208+akira-trinity@users.noreply.github.com>
2022-09-01 19:32:37 +0200

    v1.1.0

--- launcher.py
+++ launcher.py
@@ -1,11 +1,14 @@
 # IMPORTS
 #---------------------------------------------------
 
+from ctypes import LittleEndianStructure
+from http import client
 import tkinter
 import tkinter.font as font
 from PIL import Image, ImageTk
 from lib import gameEasyMode, gameMediumMode, gameHardMode, rpc
 import sys
+import yaml
 import time
 
 #---------------------------------------------------
@@ -17,8 +20,9 @@ import time
 # Functions
 #----------------------------------
 
-def startEasy():
 
+def startEasy():
+    
     gameEasyMode.easyGame()
 
 
@@ -28,11 +32,62 @@ def startMedium():
 
 
 def startHard():
-    
+
     gameHardMode.hardGame()
 
 
+def saveSettings(rpcVar, volumeValue, settingsData):
 
+
+    settingsData["rpc"] = rpcVar.get()
+    settingsData["volume"] = volumeValue.get()/10
+
+    settingsWrite = open("data/settings.yaml", "w")
+    settingsWrite.write(yaml.dump(settingsData, default_flow_style=False))
+    settingsWrite.close()
+
+def settingsWindow():
+
+    settingapp = tkinter.Toplevel()
+    settingapp.wm_title("refl3x Settings")
+    settingapp.wm_geometry("300x200")
+    settingapp.wm_minsize(300, 200)
+    settingapp.wm_maxsize(300, 200)
+    if sys.platform == "win32":
+        settingapp.wm_iconbitmap("src/crosshairIcon.ico")
+    settingapp.config(bg="#191919")
+
+    settingsfiles = open("data/settings.yaml", "r")
+    settingsData = yaml.safe_load(settingsfiles)
+    settingsfiles.close()
+
+    rpcVar = tkinter.IntVar()
+    volumeValue = tkinter.IntVar() 
+
+    if settingsData["rpc"] == 1:
+
+        rpcChoice = tkinter.Checkbutton(settingapp, bg="#191919", fg="black", variable=rpcVar)
+        rpcChoice.place(x=10, y=10)
+        rpcChoice.select()
+
+    else:
+
+        rpcChoice = tkinter.Checkbutton(settingapp, bg="#191919", fg="black", variable=rpcVar)
+        rpcChoice.place(x=10, y=10)
+
+    rpcText = tkinter.Label(settingapp, text="Discord RPC", bg="#191919", fg="white")
+    rpcText.place(x=30, y=12)
+
+    volumetext = tkinter.Label(settingapp, text="Volume :", bg="#191919", fg="white")
+    volumetext.place(x=10, y=35)
+
+    volumeScale = tkinter.Scale(settingapp, bg="#191919", fg="white", from_=0, to=10, orient=tkinter.HORIZONTAL, variable=volumeValue)
+    volumeScale.place(x=13, y=60)
+
+    volumeScale.set(settingsData["volume"]*10)
+
+    saveButton = tkinter.Button(settingapp, text="Save settings", bg="white", fg="black", command= lambda: saveSettings(rpcVar, volumeValue, settingsData))
+    saveButton.place(x=15, y=165)
 
 #----------------------------------
 
@@ -49,6 +104,7 @@ launcherapp.maxsize(700,400)
 if sys.platform == "win32":
     launcherapp.iconbitmap("src/crosshairIcon.ico")
 launcherapp.config(bg="#632C6D")
+
 
 #------------------------------------
 
@@ -76,8 +132,11 @@ mediumMode.place(x=60, y=170)
 hardMode = tkinter.Button(launcherapp, text="Hard Mode", fg="black", bg="red", borderwidth=0, command=startHard)
 hardMode.place(x=70, y=230)
 
+settingsButton = tkinter.Button(launcherapp, text="Settings", fg="black", bg="white", command=settingsWindow)
+settingsButton.place(x=80, y=330)
+
 creditLabel = tkinter.Label(launcherapp, text="by akira :)", fg="white", bg="#191919")
-creditLabel.place(x=85, y=370)
+creditLabel.place(x=77, y=370)
 
 #-------------------------------------
 
@@ -107,11 +166,18 @@ tipsLabel['font'] = tipsFont
 
 #-------------------------------------
 
-client_id = '1012394653196230737' 
-rpc_obj = rpc.DiscordIpcClient.for_platform(client_id)
-start_time = time.mktime(time.localtime())
 
-activity = {
+settingsfiles = open("data/settings.yaml", "r")
+settingsData = yaml.safe_load(settingsfiles)
+settingsfiles.close()
+
+if settingsData["rpc"] == 1:
+
+    client_id = '1012394653196230737' 
+    rpc_obj = rpc.DiscordIpcClient.for_platform(client_id)
+    start_time = time.mktime(time.localtime())
+
+    activity = {
         "timestamps": {
                 "start": start_time
             },
@@ -120,6 +186,7 @@ activity = {
         },
         "buttons" : [{"label" : "github", "url" : "https://github.com/akira-trinity/refl3x"}]
     }
-rpc_obj.set_activity(activity)
+    rpc_obj.set_activity(activity)
+
 
 launcherapp.mainloop()
